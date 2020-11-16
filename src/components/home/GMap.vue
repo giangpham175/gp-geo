68ab24a1fface5b96db70abe013523a2d05d8784
Giang <14110040@student.hcmute.edu.vn>
2020-11-16 08:59:53 +0700

    create map component

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="map">
+        <h2>MAP</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GMap',
+
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
