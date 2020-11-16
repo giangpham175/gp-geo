59d289a2f5b9f5ebbeeef9b28d00e893521635a0
Giang <14110040@student.hcmute.edu.vn>
2020-11-16 09:58:12 +0700

    create new map

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="map">
-        <h2>MAP</h2>
+        <div id="map" class="google-map"></div>
     </div>
 </template>
 
@@ -10,12 +10,38 @@ export default {
 
     data() {
         return {
-            
+            lat: 53,
+            lng: -1
         }
+    },
+
+    methods: {
+        renderMap() {
+            const map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: this.lat, lng: this.lng},
+                zoom: 6,
+                maxZoom: 15,
+                minZoom: 3,
+                streetViewControl: false
+            })
+        }
+    },
+
+    mounted() {
+        this.renderMap()
     }
 }
 </script>
 
 <style>
-
+.google-map {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    background: #fff;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+}
 </style>
